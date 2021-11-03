1a41316f1cc6c98d78aa2a78d27e4c349c884cbc
MyroslavPro <48632241+MyroslavPro@users.noreply.github.com>
2021-11-03 18:03:30 +0200

    Updated test_lab(2 more tests)

--- lab1Quicksort/test_lab1.py
+++ lab1Quicksort/test_lab1.py
@@ -16,6 +16,12 @@ class TestingQuickSort(unittest.TestCase):
         class_test = QuickSort()
         self.assertEqual(class_test.quick_sort(array, 0, len(array)-1, order), [1, 2, 3, 4, 5, 6, 7])
 
+    def test_sorting_asc_array_in_asc_order(self):
+        array = [1, 2, 3, 4, 5, 6, 7]
+        order = "asc"
+        class_test = QuickSort()
+        self.assertEqual(class_test.quick_sort(array, 0, len(array) - 1, order), [1, 2, 3, 4, 5, 6, 7])
+        
     # descending order
     def test_sorting_desc(self):
         array = [2, 5, 1, 3, 7, 6, 4]
@@ -29,6 +35,13 @@ class TestingQuickSort(unittest.TestCase):
         class_test = QuickSort()
         self.assertEqual(class_test.quick_sort(array, 0, len(array)-1, order),  [7, 6, 5, 4, 3, 2, 1])
 
+    def test_sorting_desc_array_in_desc_order(self):
+        array = [7, 6, 5, 4, 3, 2, 1]
+        order = "desc"
+        class_test = QuickSort()
+        self.assertEqual(class_test.quick_sort(array, 0, len(array) - 1, order), [7, 6, 5, 4, 3, 2, 1])
+
+    # no input
     def test_none(self):
         array = []
         order = "asc"
